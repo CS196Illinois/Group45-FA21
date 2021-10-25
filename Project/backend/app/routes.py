0c0499750b8bd862e72571ed2a665716fa4ba0b9
Taylor <33166821+taylorziegler@users.noreply.github.com>
2021-10-25 14:16:20 -0500

    login work

--- Project/backend/app/routes.py
+++ Project/backend/app/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from app import app
+from flask_login import current_user, login_user
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "hi"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if()
+    return redirect(url_for('index'))
