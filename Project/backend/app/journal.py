827a9ea1f3d72aabf0dfc35d00cc10362894ce92
Taylor <33166821+taylorziegler@users.noreply.github.com>
2021-11-29 16:36:37 -0600

    made a dictionary post for user, migrated database changes
    - issues: importing journal causes problems, i don't know why

---
+++ Project/backend/app/journal.py
@@ -0,0 +1,14 @@
+from random import *
+from models import User
+from app import db
+class Journal():
+    entries = []
+    def __init__(self):
+        with open('entry.txt') as entryData:
+            entries = entryData.readlines()
+    def getRandomEntry(self):
+        return self.entries[random.randint(0, self.entries.length()-1)]
+    def submitEntry(self, body, prompt, user):
+        entry = User(user_id=user.get_id(), prompt=prompt, body=body)
+        db.session.add(entry)
+        db.session.commit()
