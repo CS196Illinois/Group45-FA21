e8e5f2e69c3dbe1537018b79baf706680644273b
Taylor <33166821+taylorziegler@users.noreply.github.com>
2021-11-29 18:35:31 -0600

    affirmations in progress

--- Project/backend/app/routes.py
+++ Project/backend/app/routes.py
@@ -1,32 +1,40 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
-from flask_login import logout_user
 from app.forms import LoginForm, RegistrationForm
+## import urlopen
+# import requests
+# import requests
+# from journal import Journal
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "hi"
+def home():
+    return "hello world"
 @app.route('/verify')
 def verify():
     user = current_user
     if not (current_user.is_authenticated):
         return "no user is logged in"
     return "username: " + current_user.username + "\n email: " + current_user.email
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST']) # use this route for login screen
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            return render_template('login.html', info="login unsuccessful :(", form=form)
+            return {}
+            # return render_template('login.html', info="login unsuccessful :(", form=form)
         login_user(user, remember=form.remember_me.data)
         return {'id': user.id, 'username': user.username, 'email': user.email}
         # render_template('login.html', info="login successful :) welcome, " + form.username.data, form=form)
     return render_template('login.html', info="please sign in", form=form)
-@app.route('/logout')
+@app.route('/affirmation', methods=['GET', 'POST']) # untested -- affirmations api
+def affirmation():
+    source = request.args.get("https://www.affirmations.dev")
+    # response = requests.get(url).json
+    return source
+@app.route('/logout') # use this for logout button
 def logout():
     logout_user()
     return redirect(url_for('index'))
@@ -41,9 +49,25 @@ def register():
         # flash('Congratulations, you are now a registered user!')
         return "you are registered :)"
     return render_template('register.html', title='Register', form=form)
+@app.route('/prompt', methods=['GET', 'POST'])
+def prompt():
+    journal = Journal()
+    return journal.getRandomEntry()
 
-# @app.route('/prompt', methods=['GET', 'POST'])
-# def prompt():
-    # from journal import Journal
-    # journal = Journal()
-    # return journal.getRandomEntry
+
+
+
+# from random import *
+# from models import User
+# from app import db
+class Journal():
+    entries = []
+    def __init__(self):
+        with open('entry.txt') as entryData:
+            entries = entryData.readlines()
+    def getRandomEntry(self):
+        return self.entries[random.randint(0, self.entries.length()-1)]
+    def submitEntry(self, body, prompt, user):
+        entry = User(user_id=user.get_id(), prompt=prompt, body=body)
+        db.session.add(entry)
+        db.session.commit()
